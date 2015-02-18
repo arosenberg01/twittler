91724f7e45193ae95fc6067dfcf81f5d968b02ce
Ansel Rosenberg <ansel01@gmail.com>
2015-02-18 01:34:46 -0800

    Add ability to filter tweet history by user

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
   var tweetDelay = 100;
   var tweetFilter = null;
   var tweetHistory = streams.home;
+  var lastTweet;
   
+  var generateTweets = function(tweetSelection) {
+    
+  }
+
   var each = function(collection, iterator) {
     if (Array.isArray(collection)) {
       for (var i = 0; i < collection.length; i++) {
@@ -27,7 +32,16 @@ $(document).ready(function(){
     return results;
   };
 
-  var createTweets = function() {
+  $(document.body).on('click', 'a', function() {
+    
+    var targetUser = this.className;
+    var userTweets = streams.users[targetUser];
+    console.log(targetUser + " length: " + userTweets.length);
+    tweetHistory = userTweets;
+
+  });
+
+  var createTweets = function(tweetBank) {
     var tweetData = {
       tweetList: [
       ]
@@ -35,20 +49,25 @@ $(document).ready(function(){
     
     for (var i = 1; i < maxTweets + 1; i++) {
       var currentTweet = tweetHistory[tweetHistory.length-i];
-      currentTweet.timestamp = moment(currentTweet.created_at).fromNow();
-      tweetData.tweetList.push(currentTweet);
+      if (currentTweet !== undefined) {
+        currentTweet.timestamp = moment(currentTweet.created_at).fromNow();
+        tweetData.tweetList.push(currentTweet);
+      } 
     }  
 
     var tweetSource = $("#tweet-template").html();
     var tweetTemplate = Handlebars.compile(tweetSource);
     var tweetHtml = tweetTemplate(tweetData);
-  
-    $('.tweetList').html(tweetHtml);
+    
+    if (tweetHtml !== lastTweet) {
+      $('.tweetList').html(tweetHtml);
+      lastTweet = tweetTemplate(tweetData);
+    }
+    
   } 
 
-
   var showTweets = setInterval(function() {
-      createTweets();
+      createTweets(tweetHistory);
   }, tweetDelay);
 
 });
