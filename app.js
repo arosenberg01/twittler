60669ad3770a555fbf4dd201661de1f40ebff8fc
Ansel Rosenberg <ansel01@gmail.com>
2015-02-17 16:20:20 -0800

    Re-implement scrolling tweet list

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){  
-
+  var tweetCount = 0;
+  var maxTweets = 5;
+  
   var createTweet = function() {
+    var currentTweet = streams.home[streams.home.length-1];
+    currentTweet.timestamp = moment(currentTweet.created_at).fromNow();
+
     var tweetData = {
       tweetList: [
-        streams.home[streams.home.length-1]
+        currentTweet
       ]
     }
-    
     var tweetSource = $("#tweet-template").html();
     var tweetTemplate = Handlebars.compile(tweetSource);
-    var html = tweetTemplate(tweetData);
+    var tweetHtml = tweetTemplate(tweetData);
   
-    $('.tweetList').append(html);
+    $('.tweetList').prepend(tweetHtml);
   }
 
-  createTweet();
+  var removeTweet = function() {
+    var $li = $('li');
+      $li[$li.length-1].remove();
+  }
+
+  var startTweets = setInterval(function() {
+    if (tweetCount >= maxTweets) {
+      removeTweet();
+    }
+    createTweet();
+    tweetCount++
+  }, 1000);
+
+
 
 });
 
